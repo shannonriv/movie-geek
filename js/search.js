531b4ddf7665bc882c1e1272391c8cbfa9768d6f
kathy <katherine.lef@hotmail.com>
2018-01-16 19:19:07 -0500

    filtrando data movies

---
+++ js/search.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  var inputFld = $('#titleFld');
+  var searchBtn = $('#searchBtn');
+
+  searchBtn.click(handleSearchBtn);
+  function handleSearchBtn() {
+    var title = inputFld.val();
+    console.log(title)
+    var url = 'http://www.omdbapi.com/?&apikey=5bb8748d&s=' + title;
+    $.ajax({
+      url: url,
+      success: renderMovies
+    });
+  }
+
+  function renderMovies(response) {
+    console.log(response);
+    var movieList = $('#movieList');
+    movieList.empty();
+    for(var m in response.Search) {
+      var movie = response.Search[m];
+      var li = $('<li class="list-group-item">');
+      var img = $('<img src="'+movie.Poster+'" with="50px">');
+      li.append(img);
+      li.append(movie.Title);
+      movieList.append(li);
+    }
+
+  }
+})
